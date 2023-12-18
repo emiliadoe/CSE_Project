38baa8cb9475b797c4fde33d7ef3f3f53331f0ff
emiliadoe <emilia.d@web.de>
2023-12-18 14:21:43 +0300

    Login Image right size

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const LoginPage = () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Image source={require('../assets/icons/login.jpg')} /> 
+    <Image source={require('../assets/icons/login.jpg')}
+        style={styles.image}
+         /> 
   </View>
 );
 
-/* Mockpage.navigationOptions = {
-  tabBarIcon: ({ tintColor }) => (
-    <Icon name="search" size={20} color={tintColor} />
-  ),
-}; */
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    width: Dimensions.get('window').width, // Set the width to the screen width
+    height: Dimensions.get('window').height, // Set the height to the screen height
+  },
+});
 
 export default LoginPage;
